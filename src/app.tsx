2691006de8ea5d4904d2cdeb3d4f9ec72a573cbc
Daniel Vecchi <dvecchi@andplus.com>
2021-09-24 12:37:05 -0400

    init

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './app.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <div>
+        content test
+      </div>
+    </div>
+  );
+}
+
+export default App;
