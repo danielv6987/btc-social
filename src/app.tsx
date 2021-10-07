86ff6d454aebf7660829b95285b4cc52b57b5de0
Daniel Vecchi <dvecchi@andplus.com>
2021-10-07 13:53:49 -0400

    test

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './app.module.scss';
+import { Announcement } from 'kagr-ui';
 
 function App() {
   return (
     <div className={styles.app}>
       <div>
         content test test test test test
+          <Announcement
+              type="secondary"
+              hasExitIcon={true}
+          />
       </div>
     </div>
   );
